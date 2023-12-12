f8aa9c1fa568a40f944e0c2a1bdb4cf7a8b99404
skmajumder <skmajumder.cse@gmail.com>
2023-12-13 00:29:56 +0600

    using object as key

--- question-1.js
+++ question-1.js
@@ -1,6 +1,24 @@
-let user = { name: "Leo Messi" };
-const userList = [user];
-user = null;
+// * using object as key
 
-console.log(user);
-console.log(userList);
+const a = {},
+  b = { key: "b" },
+  c = { key: "c" };
+
+a[b] = 123;
+a[c] = 456;
+
+console.log(a[b]);
+
+// (function () {
+//   const a = (b = 3);
+// })();
+
+// console.log("a defined? " + typeof a);
+// console.log("b defined? " + typeof b);
+
+// let user = { name: "Leo Messi" };
+// const userList = [user];
+// user = null;
+
+// console.log(user);
+// console.log(userList);
