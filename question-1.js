802afe2f9cbbc825772a7cc0396aafa1049ec1c8
skmajumder <skmajumder.cse@gmail.com>
2023-12-13 13:00:01 +0600

    hoisting and closure

--- question-1.js
+++ question-1.js
@@ -1,10 +1,10 @@
-for (var i = 0; i < 4; i++) {
-    setTimeout(() => console.log(i), 5);
-}
+// for (var i = 0; i < 4; i++) {
+//   setTimeout(() => console.log(i), 5);
+// }
 
-for (let i = 0; i < 4; i++) {
-  setTimeout(() => console.log(i), 5);
-}
+// for (let i = 0; i < 4; i++) {
+//   setTimeout(() => console.log(i), 5);
+// }
 
 // console.log(a);
 // console.log(b);
