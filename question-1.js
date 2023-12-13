829c67c56ece1a766e74a665d28043b50d51aeaf
skmajumder <skmajumder.cse@gmail.com>
2023-12-13 13:34:38 +0600

    js this keyword

--- question-1.js
+++ question-1.js
@@ -1,3 +1,14 @@
+const income = {
+  skills: 108,
+  monthly() {
+    return this.skills * 108;
+  },
+  yearly: () => 888 * this.skills,
+};
+
+console.log(income.monthly());
+console.log(income.yearly());
+
 // for (var i = 0; i < 4; i++) {
 //   setTimeout(() => console.log(i), 5);
 // }
