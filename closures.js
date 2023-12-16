0d130c564ca163eae1ecc7be63a41a06a52218bb
skmajumder <skmajumder.cse@gmail.com>
2023-12-16 18:13:32 +0600

    Closures

--- closures.js
+++ closures.js
@@ -1,1 +1,55 @@
 // * Closures
+
+function getPassingStatus(mark) {
+  const examMark = mark;
+  return () => examMark >= 40;
+}
+
+const passingStatus = getPassingStatus(80);
+console.log(passingStatus());
+
+// function grandParent() {
+//   let p = 0;
+//   function parent() {
+//     const x = 9;
+//     const y = 10;
+
+//     function children() {
+//       console.log(x, p);
+//     }
+//     console.dir(children);
+
+//     children();
+//   }
+//   parent();
+// }
+
+// grandParent();
+
+// function parent() {
+//   const x = 9;
+//   const y = 10;
+
+//   function children() {
+//     console.log(x, y);
+//   }
+//   console.dir(children);
+
+//   children();
+// }
+
+// parent();
+
+// function counter() {
+//   let count = 0;
+//   return () => ++count;
+// }
+
+// const newCounter = counter();
+
+// console.log(newCounter());
+// console.log(newCounter());
+// console.log(newCounter());
+
+// console.log(globalThis)
+// console.log(window)
